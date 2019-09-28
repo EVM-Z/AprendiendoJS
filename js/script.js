eea440331d45c6b0580e5b1ff3f6d96852fb2031
EliseoVM <elyzeo.vm@gmail.com>
2019-09-28 13:02:38 -0500

    Operador && y || y el Ternario  v2

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-let dinero=300;
-let totalCarrito=500;
-let tarjeta=true;
+// Ternario
 
-if((dinero>totalCarrito) || tarjeta){
-    console.log('Pago Correcto');
+let puntaje;
+
+if(puntaje){
+    console.log('Si hay puntaje');
 }
 else{
-    console.log('Fondos Insuficientes');
-}
-
-
-let hora=14;
-if(hora>0 && hora<=12){
-    console.log('Buenos dias');
-}
-else if(hora>12 && hora<=18){
-    console.log('Buenas tardes');
-}
-else if(hora>18 && hora<=24){
-    console.log('Buenas noches');
-}
-else{
-    console.log('Horario no valido');
+    console.log('No hay puntaje');
 }
