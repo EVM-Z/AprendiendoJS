9461287e221b67a4eb963382eba95d7a0a40216a
EliseoVM <elyzeo.vm@gmail.com>
2019-09-30 11:23:05 -0500

    Scope - Variables globales y locales

--- js/script.js
+++ js/script.js
@@ -1,54 +1,12 @@
-// Arreglo
-const pendientes = ['Tarea', 'Comer', 'Proyecto', 'Estudiar Javascrit'];
+// Scope
 
-// Arreglo con objetos
-const carrito=[
-    {id:1, producto: 'Libro'},
-    {id:1, producto: 'Camisa'},
-    {id:3, producto: 'Disco'}
-];
+// Variable global let
+let musica='Rock';
 
-// Objeto
-let automovil={
-    modelo: 'Camaro',
-    motor: 6.0,
-    anio: 1967,
-    marca: 'Chevrolet'
+if(musica){
+    // Variable local dentro del ciclo
+    let musica='Grunge';
+    console.log('Dentro del if '+musica);
 }
 
-// Accedemos a los datos de forma individual
-Object.values(automovil).forEach(info => {
-    console.log(info);
-});
-
-console.log('=====');
-
-// Crea una copia de un arreglo
-Object.values(automovil).map(info =>{
-    console.log(info);
-});
-
-// Accede a los valores de un objeto
-Object.values(automovil).forEach(info => {
-    console.log(info);
-});
-
-console.log('=====');
-
-Object.values(automovil).map(info => {
-    console.log(info);
-});
-
-console.log('=====');
-
-// Trae todos los pendientes que tienes
-// for(let i=0; i<pendientes.length; i++){
-//     console.log(pendientes[i]);
-// }
-
-console.log('=====');
-
-// Una forma mas corta que la anterior
-// for(pendiente of pendientes){
-//     console.log(pendiente);
-// }
+console.log('Fuera del if '+musica);
