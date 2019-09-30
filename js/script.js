fee0801b24ae39507e173abb0bf532a33012b709
EliseoVM <elyzeo.vm@gmail.com>
2019-09-30 11:40:20 -0500

    Object Destructuring

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-// Scope
+// Destructuring
 
-// Variable global let
-let musica='Rock';
-
-if(musica){
-    // Variable local dentro del ciclo
-    let musica='Grunge';
-    console.log('Dentro del if '+musica);
+const cliente={
+    nombre: 'Alejandra',
+    tipo: 'Premium',
+    datos:{
+        ubicacion:{
+            ciudad: 'Mexico',
+            pais: 'Mexico'
+        },
+        cuenta:{
+            desde: '10-12-2012',
+            saldo: 4000
+        }
+    },
+    gustos:{
+        musica:['Trance', 'Rock']
+    }
 }
 
-console.log('Fuera del if '+musica);
+console.log(cliente);
+console.log('=====');
+
+// Crear la variable
+// Forma antigua
+// const nombreCliente=cliente.nombre,
+//     tipoCliente=cliente.tipo,
+//     ubicacionCliente=cliente.datos.ubicacion;
+
+// console.log(ubicacionCliente);
+console.log('=====');
+
+
+
+// Forma Nueva
+// let {nombre, tipo}=cliente;
+// let {datos:{ubicacion:{ciudad}}}=cliente;
+// console.log(ciudad);
+
+// Extraemos un dato especifico del objeto
+// let {datos:{cuenta:{saldo}}}=cliente;
+// console.log(saldo);
+
+let {gustos:musica}=cliente;
+console.log(musica);
