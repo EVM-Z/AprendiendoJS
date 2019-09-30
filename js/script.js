66b8141a3c916bcbb6e19b1053e7ecc855cd783d
EliseoVM <elyzeo.vm@gmail.com>
2019-09-30 12:34:09 -0500

    Filter find reduce

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-// Object Literal Enhacement
+const personas=[
+    {nombre:'Juan', edad:20},
+    {nombre:'Pablo', edad:50},
+    {nombre:'Alejandra', edad:23, aprendiendo: 'JavaScript'},
+    {nombre:'Karen', edad:28},
+    {nombre:'Miguel', edad:33}
+];
 
-const banda='Metalica',
-    genero='Heavy Metal',
-    canciones=['Master', 'Master de Master', 'Jefe'];
+// Obtener las personas mayores de 25 años
+const mayores=personas.filter(personas=>personas.edad>25);
+console.log(mayores);
+console.log('=====');
 
-// Forma antigua de hacerlo
-const metallica={
-    banda:banda,
-    genero:genero,
-    canciones:canciones
-}
+// Extraer informacion de Alejandra
+const alejandra=personas.find(personas=>personas.nombre==='Alejandra');
+let {aprendiendo}=alejandra;
+console.log(aprendiendo);
+console.log('=====');
 
-console.log(metallica);
-
-// Forma nueva de acceder a los valores
-const metallica={banda, genero, canciones}
-
-console.log(metallica);
+// Suma total de todas las edades de las personas
+// Reduce
+let total=personas.reduce((edadTotal, peronas)=>{
+    return edadTotal+peronas.edad;
+}, 0);
+console.log(total/personas.length);
